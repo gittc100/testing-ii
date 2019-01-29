7debd469852239c73cf654374eeb6b76c05216b4
Thomas Claydon <tomclaydon102@gmail.com>
2019-01-29 17:46:30 -0500

    working on testing

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,75 @@ import Display from "./Display/Display.js";
 import "./App.css";
 
 class App extends Component {
-  state = {};
+  constructor() {
+    super();
+    this.state = {
+      balls: 0,
+      strikes: 0,
+      fouls: 0,
+      hits: 0
+    };
+  }
+
+  reset = () => {
+    this.setState({
+      ...this.state,
+      balls: 0,
+      strikes: 0,
+      fouls: 0,
+      hits: 0
+    });
+  };
+
+  check = (item, name) => {
+    let { strikes, balls } = this.state;
+    console.log(item);
+    if (strikes === 2 || balls === 3) {
+      this.reset();
+    } else {
+      let val = item + 1;
+      console.log(val);
+      this.setState({
+        ...this.state,
+        [name]: val
+      });
+    }
+  };
+
+  // back = () => {
+  //   return this.props.history.push("/");
+  // };
+
+  addStrike = event => {
+    event.preventDefault();
+    let name = "strikes";
+    let { strikes } = this.state;
+    this.check(strikes, name);
+  };
+
+  addBall = () => {
+    let name = "balls";
+    let { balls } = this.state;
+    this.check(balls, name);
+  };
+
+  addFoul = () => {
+    let name = "fouls";
+    let { fouls } = this.state;
+    this.check(fouls, name);
+  };
+
+  addHit = () => {
+    let name = "hits";
+    let { hits } = this.state;
+    this.check(hits, name);
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <nav>
+          <nav className="navBar">
             <NavLink to="/" exact>
               Display
             </NavLink>
@@ -18,8 +81,23 @@ class App extends Component {
           </nav>
         </header>
         <main>
-          <Route path="/" component={Display} exact/>
-          <Route path="/dash-board" component={DashBoard} />
+          <Route
+            path="/"
+            render={props => <Display {...props} state={this.state} />}
+            exact
+          />
+          <Route
+            path="/dash-board"
+            render={props => (
+              <DashBoard
+                {...props}
+                addStrike={this.addStrike}
+                addBall={this.addBall}
+                addFoul={this.addFoul}
+                addHit={this.addHit}
+              />
+            )}
+          />
         </main>
       </div>
     );
--- client/src/Display/Display.js
+++ client/src/Display/Display.js
@@ -1,11 +1,22 @@
-import React, {Component} from 'react';
-
-class Display extends Component{
-    render(){
-        return(
-            
-        );
-    }
+import React, { Component } from "react";
+import "../App.css";
+class Display extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <>
+        <div className="display">
+          <h2>Player At Bat</h2>
+          <p>Balls: {this.props.state.balls}</p>
+          <p>Strikes: {this.props.state.strikes}</p>
+          <p>Fouls: {this.props.state.fouls}</p>
+          <p>Hits: {this.props.state.hits}</p>
+        </div>
+      </>
+    );
+  }
 }
 
 export default Display;
